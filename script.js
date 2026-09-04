35be6e4b1a9d67ac96c5748439cfd5d0c724d5cb
c20245279garcia-ops <c20245279.garcia@csav.edu.ph>
2026-09-04 17:32:12 +0800

    addEventListener

--- script.js
+++ script.js
@@ -23,17 +23,38 @@ function greet(greetings,names=[]) {
 console.log(greet("mayong aga",["franz","bai","soy","tol","Baby" ]));
 
 const heading = document.querySelector("h1");
+heading.textContent = "Joseph Garcia";
 heading.style.color = "purple"
 heading.style.backgroundColor = "yellow"
-heading.textContent = "Joseph Garcia";
-heading.style.fontSize = "10px";
+heading.style.fontSize = "60px";
 console.log(heading.textContent);
 
+heading.addEventListener("click", function(){
+    heading.style.color = "red";
+    console.log("Heading was clicked!");
+});
 const contactHeading = document.querySelector("#contact h2");
 console.log(contactHeading);
 
 const projectHeading = document.querySelector("#services h2");
 console.log(projectHeading);
+
+const toggleButton = document.querySelector(`#switch`);
+const body = document.querySelector(`body`);
+let isOff = false;
+
+toggleButton.addEventListener("click",function () {
+    isOff = !isOff;
+
+    if (isOff){
+        body.style.backgroundColor = "Black";
+        body.style.color = "white";
+} else {
+        body.style.backgroundColor = "white";
+        body.style.color = "black";
+
+}
+});
 
 
 
