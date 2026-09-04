815eb25ffd5780a0e78a53b1de89c2c57578873e
c20245279garcia-ops <c20245279.garcia@csav.edu.ph>
2026-09-04 14:41:25 +0800

    Variables (let and const)

--- script.js
+++ script.js
@@ -1,2 +1,19 @@
 console.log ("Hello world.");
 console.log("From script.js file.");
+
+const myName = "Franz";
+let age = 21;
+let number = "09936063268";
+let address = "Brgy 2, Manapla"
+let addnumber = 67;
+
+
+console.log (`Name : ${myName}`); 
+console.log (`age : ${age} `);
+console.log (`number : ${number}`);
+console.log (`address : ${address} `);
+console.log(addnumber);
+
+
+
+
