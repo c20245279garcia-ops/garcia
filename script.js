cfb99ea2b78a7ba244617a9a8531c36bc8c28cab
c20245279garcia-ops <c20245279.garcia@csav.edu.ph>
2026-09-04 15:40:22 +0800

    functions

--- script.js
+++ script.js
@@ -12,8 +12,13 @@ console.log (`Name : ${myName}`);
 console.log (`age : ${age} `);
 console.log (`number : ${number}`);
 console.log (`address : ${address} `);
-console.log(addnumber);
+console.log (addnumber);
 
-
-
+function greet(greetings,names=[]) {
+    if(Array.isArray(names)){
+     names = names.join([","]);
+}
+     return `${greetings}  sa emu, ${names}` ;
+}
+console.log(greet("mayong aga",["franz","bai","soy","tol","Baby" ]));
 
